1d71f5c0c407594bda0b878193d2b84b4a264752
Dmitry <tixxxon@gmail.com>
2018-01-02 16:48:48 +0300

    - some change

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -3,7 +3,10 @@ $(function () {
   var prev_s = $(document).scrollTop();
   var w_height = $(window).height();
 
+  let paralax_background = $('.background-paralax');
   var paralax_sl = $('.background-paralax .slogan');
+
+  let def_sl_spos = Number(paralax_sl.css('top').slice(0, -2));
 
   var sl_y_range = {
     min: Number(paralax_sl.css('top').slice(0, -2)),
@@ -21,15 +24,19 @@ $(function () {
     var cur_s = $(this).scrollTop();
     var delta = prev_s - cur_s;
 
+    if (cur_s < paralax_background.height()) {
+      requestAnimationFrame(function () {
+        // paralax_background.css('background-position-y', (0 - (cur_s * 0.3)) + 'px');
+        // paralax_background.css('height', (0 - (cur_s * 0.3)) + 'px');
+      });
+    }
     if (cur_s < sl_y_range.max) {
-      var sl_y = Number(paralax_sl.css('top').slice(0, -2));
-      sl_y -= delta / 2;
-      paralax_sl.css({
-        'top': `${sl_y}px`
-      })
+      requestAnimationFrame(function () {
+        paralax_sl.css('top', (def_sl_spos + (cur_s * 0.5)) + 'px');
 
-      if (prev_s > cur_s && sl_y < sl_y_range.min) paralax_sl.css('top', `${sl_y_range.min}px`)
-      if (prev_s < cur_s && sl_y > sl_y_range.max) paralax_sl.css('top', `${sl_y_range.max}px`)
+        if (prev_s > cur_s && sl_y < sl_y_range.min) paralax_sl.css('top', `${sl_y_range.min}px`);
+        if (prev_s < cur_s && sl_y > sl_y_range.max) paralax_sl.css('top', `${sl_y_range.max}px`);
+      });
     }
     prev_s = cur_s;
   });
