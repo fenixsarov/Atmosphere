313bdd05cd64389d41454feb54b6acf8c0a4e162
Dmitry <tixxxon@gmail.com>
2018-01-08 20:01:10 +0300

    - добавлены актуальные фото - изменены стили - начальный статичный шаблон завершён ( без мобильной версии ) - удалены старые изображения и стили - добавлены шрифты (и разнесены по различным папкам)

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -8,14 +8,20 @@ $(function () {
   let $prlx_slogan = $('#atm-header .atm-slogan');
   let $prlx_main = $('#main-content .parallax-img');
 
+  let $carousel = $("#scenery-carousel");
+
+  setInterval(function () {
+    slideTo($carousel);
+  }, 1500);
+
   let slogan_start = $prlx_slogan[0].offsetTop;
   let header_height = w_height;
 
-  $header.height(w_height);
+  // $header.height(w_height);
 
   $(window).on('resize', function () {
     w_height = $(window).height();
-    $header.height(w_height);
+    // $header.height(w_height);
     header_height = w_height;
   });
 
@@ -26,11 +32,11 @@ $(function () {
     requestAnimationFrame(function () {
       if (cur_s <= header_height) {
         headerParallax(cur_s);
-        $prlx_main.css('position', 'absolute');
+        // $prlx_main.css('position', 'absolute');
       }
       if (cur_s > header_height) {
         // mainContentParallax( cur_s );
-        $prlx_main.css('position', 'fixed');
+        // $prlx_main.css('position', 'fixed');
       }
     });
 
@@ -39,10 +45,13 @@ $(function () {
 
   function headerParallax(c_scroll) {
     $prlx_img1.css('top', (c_scroll * 0.5) + 'px');
-    $prlx_slogan.css('top', (slogan_start + c_scroll * 0.3) + 'px');
+    // $prlx_slogan.css('top', (slogan_start + c_scroll * 0.3) + 'px');
   }
   function mainContentParallax(c_scroll) {
     $prlx_main.css('top', (c_scroll - header_height) + 'px');
+  }
+  function sliderSlide() {
+
   }
 });
 function hidePreload() {
@@ -57,4 +66,26 @@ function startPage() {
   $('.background-paralax .slogan').animate({
     opacity: '1'
   }, 700);
+}
+function slideTo(carousel, number) {
+  let item = carousel.find('.item');
+  let item_count = item.length;
+
+  item.each(function (i, e) {
+    let active = $(e).hasClass('active');
+
+    let w = carousel.width();
+    let pos = $(e).position();
+
+    // if (active && (item_count - 1) == i) {
+    //   $(e).animate({
+    //     'left': `${i * 100}%`
+    //   }, 500);
+    // } else {
+    //   $(e).animate({
+    //     'left': `-${}`
+    //   }, 500);
+    // }
+  });
+  // console.log(item.position());
 }
