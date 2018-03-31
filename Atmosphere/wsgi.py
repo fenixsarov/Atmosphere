8676e2a319df387ed459c747c308ba828f4eba2c
fenixsarov <fenixsarov@gmail.com>
2018-04-01 02:45:53 +0300

    Modified wsgi.py for production-mode

--- Atmosphere/wsgi.py
+++ Atmosphere/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from django.conf import settings
+
+
+if settings.DEBUG == False:
+    PROJECT_DIR = '/home/atmosphera/Atmosphere'
+    PACKAGE_DIR = os.path.join(PROJECT_DIR, '/home/atmosphera/Atmosphere_env/lib/python3.5/site-packages')
+    sys.stdout = sys.stderr
+
+# os.environ["DJANGO_SETTINGS_MODULE"] = "Atmosphere.settings"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Atmosphere.settings")
+
+if settings.DEBUG == False:
+    sys.path.insert(0, PROJECT_DIR)
+    sys.path.insert(1, PACKAGE_DIR)
+    site.addsitedir(PACKAGE_DIR)
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "Atmosphere.settings"
 
 application = get_wsgi_application()
