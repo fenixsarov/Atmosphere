93f96cd040178f10df6fb0474e8dd6921daeb458
fenixsarov <fenixsarov@gmail.com>
2018-09-11 23:51:52 +0300

    Update fill_db function

--- mainApp/management/commands/fill_db.py
+++ mainApp/management/commands/fill_db.py
@@ -203,7 +203,36 @@ class Command(BaseCommand):
             grad = Graduations(**grad)
             grad.save()
 
-        blogArcticle = []
+        blogArcticle = [
+            {
+                "title": "\u041f\u0435\u0440\u0432\u0430\u044f \u0437\u0430\u043f\u0438\u0441\u044c \u0431\u043b\u043e\u0433\u0430",
+                "desc": "\u0424\u043e\u0442\u043e\u043e\u0442\u0447\u0451\u0442 \u043e \u043f\u0435\u0440\u0432\u043e\u043c \u0434\u043d\u0435 \u0441\u043e\u0440\u0435\u0432\u043d\u043e\u0432\u0430\u043d\u0438\u0439 \"\u041b\u0443\u0447\u0448\u0438\u0439 \u0414\u043e\u043c\u043e\u0432\u043e\u0439 \u0433\u043e\u0434\u0430\"",
+                "desc_image": "static/images/upload_imgs/school_img_10_Sqt2LUH.jpg",
+                "content": "\u041d\u0443 \u0430 \u0442\u0443\u0442 \u0431\u0443\u0434\u0435\u0442 \u043a\u0430\u043a\u043e\u0439-\u0442\u043e \u043a\u043e\u043d\u0442\u0435\u043d\u0442 \u0431\u043b\u043e\u0433\u0430",
+                "public_date": "2018-09-10"
+            },
+             {
+                "title": "\u0412\u0442\u043e\u0440\u0430\u044f \u0437\u0430\u043f\u0438\u0441\u044c \u0431\u043b\u043e\u0433\u0430",
+                "desc": "\u0425\u043e\u0442\u0438\u043c \u0440\u0430\u0441\u0441\u043a\u0430\u0437\u0430\u0442\u044c \u0432\u0430\u043c \u043e \u043d\u0430\u0448\u0435\u043c \u0441\u0435\u043a\u0440\u0435\u0442\u0435, \u043a\u043e\u0442\u043e\u0440\u044b\u0439 \u043d\u0438\u043a\u0442\u043e \u043d\u0435 \u0437\u043d\u0430\u0435\u0442",
+                "desc_image": "static/images/upload_imgs/school_img_1.jpg",
+                "content": "\u041d\u0443 \u0430 \u0442\u0443\u0442 \u0431\u0443\u0434\u0435\u0442 \u043a\u0430\u043a\u043e\u0439-\u0442\u043e \u043a\u043e\u043d\u0442\u0435\u043d\u0442 \u0431\u043b\u043e\u0433\u0430",
+                "public_date": "2018-09-11"
+            },
+            {
+                "title": "\u0422\u0440\u0435\u0442\u044c\u044f \u0437\u0430\u043f\u0438\u0441\u044c \u0431\u043b\u043e\u0433\u0430",
+                "desc": "\u0410 \u0432\u044b \u0432\u0438\u0434\u0435\u043b\u0438 \u044d\u0442\u043e? \u0410 \u043c\u044b \u044d\u0442\u043e \u0437\u0430\u0441\u043d\u044f\u043b\u0438!",
+                "desc_image": "static/images/upload_imgs/school_img_4.jpg",
+                "content": "\u041d\u0443 \u0430 \u0442\u0443\u0442 \u0431\u0443\u0434\u0435\u0442 \u043a\u0430\u043a\u043e\u0439-\u0442\u043e \u043a\u043e\u043d\u0442\u0435\u043d\u0442 \u0431\u043b\u043e\u0433\u0430",
+                "public_date": "2018-07-10"
+            },
+            {
+                "title": "\u0427\u0435\u0442\u0432\u0451\u0440\u0442\u0430\u044f \u0441\u0442\u0430\u0442\u044c\u044f \u0431\u043b\u043e\u0433\u0430",
+                "desc": "\u041e\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0443\u0442",
+                "desc_image": "static/images/upload_imgs/school_img_12.jpg",
+                "content": "\u0422\u0435\u043a\u0441\u0442",
+                "public_date": "2018-08-15"
+            }
+        ]
 
         for ba in blogArcticle:
             ba = BlogArticle(**ba)
