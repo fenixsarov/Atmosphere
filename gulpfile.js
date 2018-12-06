14adfd400b6b03bf0cbbdb2a718e67bee0973c94
Dmitry Tishkin <tixxxon@gmail.com>
2018-12-06 20:12:46 +0300

    - watch task in  gulpfile fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,35 @@
   Compress это задача, описаная в файле gulpfile.js
 */
 
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
+var gulp = require( 'gulp' );
+var minify = require( 'gulp-minify' );
+var concat = require( 'gulp-concat' );
+var imagemin = require( 'gulp-imagemin' );
 
 var js_src = [
   "static/javascripts/src/reserv_widget.js",
   "static/javascripts/src/main.js"
 ];
-gulp.task('build_js', function () {
-  return gulp.src(js_src)
-    .pipe(concat('main.js'))
-    .pipe(minify({
+gulp.task( 'build_js', function () {
+  return gulp.src( js_src )
+    .pipe( concat( 'main.js' ) )
+    .pipe( minify( {
       'noSource': true,
       'ext': {
         'min': ".min.js"
       }
-    }))
-    .pipe(gulp.dest('static/javascripts'));
-});
+    } ) )
+    .pipe( gulp.dest( 'static/javascripts' ) );
+} );
 
-gulp.task('imagemin', function () {
-  return gulp.src('static/images/graduations/*.jpg')
-    .pipe(imagemin({
+gulp.task( 'imagemin', function () {
+  return gulp.src( 'static/images/graduations/*.jpg' )
+    .pipe( imagemin( {
       verbose: true
-    }))
-    .pipe(gulp.dest('static/images/graduations/compressed'));
-});
+    } ) )
+    .pipe( gulp.dest( 'static/images/graduations/compressed' ) );
+} );
 
-gulp.task('watch', function () {
-  gulp.watch(js_src, ['build_js']);
-});
+gulp.task( 'watch', [ 'build_js' ], function () {
+  gulp.watch( js_src, [ 'build_js' ] );
+} );
