d55cf71dbcecd41ad3a5820a119a9216a3cda378
fenixsarov <fenixsarov@gmail.com>
2018-12-13 01:05:01 +0300

    Added robots.txt and fixed some  w3c -validator errors

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 # from mainApp import views
 from mainApp.views import *
+from django.views.generic import TemplateView
 
 urlpatterns = (
     # url(r'^$', views.main, name='index'),
@@ -8,22 +9,18 @@ urlpatterns = (
     url(r'^halls/$', Halls.as_view()),
     url(r'^halls/hallschange/$', HallsChange.as_view(), name='hallschange'),
     url(r'^graduations/$', Graduations.as_view()),
-    url(r'^graduations/graduationschange/$',
-        GraduationsChange.as_view(),
+    url(r'^graduations/graduationschange/$', GraduationsChange.as_view(),
         name='graduationschange'),
     url(r'^sessions/$', Sessions.as_view()),
-    url(r'^halls/sessionschange/$',
-        SessionsChange.as_view(),
+    url(r'^halls/sessionschange/$', SessionsChange.as_view(),
         name='sessionschange'),
     url(r'^school/$', School.as_view()),
     url(r'^school/photo/$', PhotoSchool.as_view()),
     url(r'^school/study_photography/$', StudyPhotography.as_view()),
     url(r'^masterclass/$', MasterClass.as_view()),
-    url(r'^masterclass/(?P<pk>\d+)/$',
-        SingleMasterClass.as_view(),
+    url(r'^masterclass/(?P<pk>\d+)/$', SingleMasterClass.as_view(),
         name='single_mc'),
-    url(r'^masterclass/masterclasschange/$',
-        MasterClassChange.as_view(),
+    url(r'^masterclass/masterclasschange/$', MasterClassChange.as_view(),
         name='masterclasschange'),
     url(r'^reserved/form/$', ReservedForm.as_view(), name='reserved'),
     url(r'^events/$', Events.as_view()),
@@ -34,4 +31,5 @@ urlpatterns = (
     url(r'^blog/blogchange/$', BlogChange.as_view(), name='blogchange'),
     url(r'^blog/(?P<pk>\d+)/$', BlogSingleArticle.as_view(), name='blog_article'),
     url(r'^reserve/$', ReservedForm.as_view()),
+    url(r'^robots\.txt$', TemplateView.as_view( template_name='robots.txt', content_type='text/plain' ))
 )
