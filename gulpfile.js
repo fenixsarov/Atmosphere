661fa2cd1d8913aea24813c6fdd2c5aefd8a3bcf
Dmitry Tishkin <tixxxon@gmail.com>
2019-01-12 11:54:32 +0300

    - немного изменено описание gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,42 @@
-/*
+/**
   Для запуска необходимо установить node.js http://nodejs.org
   после этого, запустить в папке проекта команду npm install (поставятся все зависимости)
   правда не помню нужно ли устанавливать gulp глобально, если он это напишет выполнить (npm install gulp -g)
   
-  После всего этого, поисле изменения js кода в static/javascripts/src выполнить команду gulp compress 
-  Compress это задача, описаная в файле gulpfile.js
+  Для запуска сборки js файлов из исходников, необходимо выполнить команду gulp build_js ( она выполнится единожды )
+    каждый раз после изменения файлов в src нужно будет выполнять команду
+  Для того, что бы происходило автоматическое собирание файлов при изменении необходимо выполнить gulp watch
 */
 
-var gulp = require( 'gulp' );
-var minify = require( 'gulp-minify' );
-var concat = require( 'gulp-concat' );
-var imagemin = require( 'gulp-imagemin' );
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 
-var js_src = [
+const js_src = [
   "static/javascripts/src/reserv_widget.js",
   "static/javascripts/src/main.js"
 ];
-gulp.task( 'build_js', function () {
-  return gulp.src( js_src )
-    .pipe( concat( 'main.js' ) )
-    .pipe( minify( {
+gulp.task('build_js', function () {
+  return gulp.src(js_src)
+    .pipe(concat('main.js'))
+    .pipe(minify({
       'noSource': true,
       'ext': {
         'min': ".min.js"
       }
-    } ) )
-    .pipe( gulp.dest( 'static/javascripts' ) );
-} );
+    }))
+    .pipe(gulp.dest('static/javascripts'));
+});
 
-gulp.task( 'imagemin', function () {
-  return gulp.src( 'static/images/graduations/*.jpg' )
-    .pipe( imagemin( {
+gulp.task('imagemin', function () {
+  return gulp.src('static/images/graduations/*.jpg')
+    .pipe(imagemin({
       verbose: true
-    } ) )
-    .pipe( gulp.dest( 'static/images/graduations/compressed' ) );
-} );
+    }))
+    .pipe(gulp.dest('static/images/graduations/compressed'));
+});
 
-gulp.task( 'watch', [ 'build_js' ], function () {
-  gulp.watch( js_src, [ 'build_js' ] );
-} );
+gulp.task('watch', ['build_js'], function () {
+  gulp.watch(js_src, ['build_js']);
+});
